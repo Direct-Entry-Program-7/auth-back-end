520e92671f665f23a6507c3cb30256f5cd21a4f4
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-10-15 17:00:18 +0530

    Implement authnication

---
+++ src/main/java/lk/ijse/dep7/authbackend/api/AuthServlet.java
@@ -0,0 +1,54 @@
+package lk.ijse.dep7.authbackend.api;
+
+import jakarta.annotation.Resource;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbException;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import lk.ijse.dep7.authbackend.dto.UserDTO;
+import lk.ijse.dep7.authbackend.security.SecurityContext;
+import lk.ijse.dep7.authbackend.service.UserService;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet(name = "AuthServlet", value = "/authenticate")
+public class AuthServlet extends HttpServlet {
+
+    @Resource(name = "java:comp/env/jdbc/cp")
+    private DataSource dataSource;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        if (request.getContentType() == null || !request.getContentType().startsWith("application/x-www-form-urlencoded")){
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid request (Not a valid content-type)");
+            return;
+        }
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (username == null || password == null){
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Bad login credentials");
+            return;
+        }
+
+        try (Connection connection = dataSource.getConnection()) {
+            UserService userService = new UserService(connection);
+
+            try {
+                UserDTO user = userService.authenticate(username, password);
+                SecurityContext.setPrincipal(user);
+            }catch (RuntimeException e){
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Bad login credentials");
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
